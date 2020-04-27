fe35583fa5f71d52983288cc15a313c057590db2
saran141998 <saravanansr98@gmail.com>
2020-04-27 08:46:16 +0530

    cardList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
@@ -8,27 +8,20 @@ class App extends Component {
     super();
     this.state = {
       monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        },
-        {
-          name: 'Dragon'
-        }
+
       ]
     };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1>{monster.name}</h1>)
-        }
+
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
