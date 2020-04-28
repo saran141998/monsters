cc29e3fc944e1f78976434207c80c751f693149d
saran141998 <saravanansr98@gmail.com>
2020-04-28 13:16:59 +0530

    FinalCommit

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,14 +6,17 @@ import { Card } from '../card/card.component'
 
 export const CardList = props => {
 
+
     return (
-        <div className='card-list'>
+
+        < div className='card-list' >
             {
+
                 props.monsters.map(monster => (
                     <Card key={monster.id} monster={monster} />
                 ))
             }
-        </div>
+        </div >
     )
 
 }
