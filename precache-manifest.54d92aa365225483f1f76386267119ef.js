0c0d0352e5e27a5511b96bc9b206534ebcc13b17
saran141998 <saravanansr98@gmail.com>
2020-04-29 11:37:14 +0530

    Updates

---
+++ precache-manifest.54d92aa365225483f1f76386267119ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92239af7685d01970bc76529036d9a0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d42cf0880319b1c09211",
+    "url": "/monsters/static/css/main.9306be1b.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42cf0880319b1c09211",
+    "url": "/monsters/static/js/main.f6979d08.chunk.js"
+  },
+  {
+    "revision": "341975cc8ff21a216c4c",
+    "url": "/monsters/static/js/runtime-main.7edf1021.js"
+  }
+]);
